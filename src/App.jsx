dd5e55a7b54547cf370f8b7178ce94acde9dfc25
Ian Graham <i4ng1999@gmail.com>
2024-11-01 13:21:15 +0000

    Initalise repository

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <h1 className="text-5xl text-center font-bold">
+        Weekly Meals App
+      </h1>
+    </div>
+  )
+}
